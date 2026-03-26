b86548b3ca36532440b3ff280d6940e4f913fabb
FLIGHTZONE SIGNALS <195437507+bgwero31@users.noreply.github.com>
2026-03-26 09:22:56 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-
 import "./globals.css"
 import AuthGate from "./AuthGate"
-import Script from "next/script"
 
 export const metadata = {
   title: "SendIt",
@@ -27,45 +25,7 @@ export default function RootLayout({ children }) {
           {children}
         </AuthGate>
 
-        {/* OneSignal SDK */}
-        <Script
-          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
-          strategy="afterInteractive"
-        />
-
-        {/* OneSignal Init */}
-        <Script id="onesignal-init" strategy="afterInteractive">
-          {`
-            window.OneSignalDeferred = window.OneSignalDeferred || [];
-
-            OneSignalDeferred.push(async function(OneSignal) {
-
-              await OneSignal.init({
-                appId: "cea0d2bd-0c2f-4126-85dc-b404c3482341",
-                notifyButton: { enable: true }
-              });
-
-              /* ================= HANDLE NOTIFICATION CLICK ================= */
-
-              OneSignal.Notifications.addEventListener("click", function(event) {
-
-                const data = event.notification.additionalData;
-
-                if (data && data.type === "task" && data.taskId) {
-
-                  console.log("Opening task:", data.taskId);
-
-                  window.location.href = "/task/" + data.taskId;
-
-                }
-
-              });
-
-            });
-          `}
-        </Script>
-
       </body>
     </html>
   )
-    }
+}
