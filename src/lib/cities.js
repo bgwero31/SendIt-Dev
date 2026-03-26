6bc6ac80a26b1dc33418150c766b1abe79ceae76
FLIGHTZONE SIGNALS <195437507+bgwero31@users.noreply.github.com>
2026-03-26 07:59:34 +0200

    Create cities.js

---
+++ src/lib/cities.js
@@ -0,0 +1,13 @@
+export const ZIM_CITIES = [
+  "Harare",
+  "Bulawayo",
+  "Mutare",
+  "Gweru",
+  "Masvingo",
+  "Chinhoyi",
+  "Kadoma",
+  "Kwekwe",
+  "Victoria Falls",
+  "Zvishavane",
+  "Beit bridge"
+]
